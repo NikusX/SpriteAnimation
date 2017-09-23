9db3cd00a88bda90180ef4d1ff371c8a558cb77f
NikusX <tigrnik3@gmail.com>
2017-09-23 19:41:55 +0300

    Start project

---
+++ SpriteAnimation/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+
+namespace SpriteAnimation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.BackColor = Color.White;
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Pen p = new Pen(Color.Black);
+            SolidBrush br = new SolidBrush(Color.Black);
+            g.DrawEllipse(p, 10, 10, 100, 100);
+            g.FillEllipse(br, 10, 10, 100, 100);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.)
+        }
+    }
+}
